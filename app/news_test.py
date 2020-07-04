d4d6d73947ab6ab4350dc87181212e31ca6f1fff
antony-guthiga <toniguthiga93@gmail.com>
2020-07-04 15:24:09 +0300

    add test class to test behavior of the class

--- app/news_test.py
+++ app/news_test.py
@@ -11,4 +11,10 @@ class NewsTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_news = News()
+        self.new_news = News('Nairametrics','Top Nigerian FinTech Apps that are leading the competition - Nairametrics','It is estimated that there are about 210-250 fintech operators/companies operating in the Nigerian space.','"https://i0.wp.com/nairametrics.com/wp-content/uploads/2018/01/FIntech.jpeg?resize=1000%2C600&ssl=1','2020-07-04T07:06:39Z','Financial technology is one of the new waves of disruptions in the financial sector, that is fuelled by the internet of things and the increasing digitalisation of the world. In the last decade, the … [+10027 chars]')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+if __name__ == '__main__':
+    unittest.main()

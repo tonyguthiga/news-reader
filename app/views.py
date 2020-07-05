c707b5559d3f5332f83f606b808b3575ec33e766
antony-guthiga <toniguthiga93@gmail.com>
2020-07-05 15:35:38 +0300

    add variable for getting news sources

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 #Views
 @app.route('/')
@@ -7,9 +8,10 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-
+    #Getting news sources
+    news_sources = get_news()
     title = 'Home - Welcome to News-reader website for the best preview of news articles'
-    return render_template('index.html',title = title)
+    return render_template('index.html', title = title, news_sources = news_sources)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):

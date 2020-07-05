cb4ff4ee0fdaec98087d134d734f39c37d5996a9
antony-guthiga <toniguthiga93@gmail.com>
2020-07-05 15:42:54 +0300

    change objects defined for News class

--- app/models/news.py
+++ app/models/news.py
@@ -1,12 +1,10 @@
 class News:
     '''
-    News class to define News Objects
+    Source class to define Source Objects
     '''
 
-    def __init__(self,name,title,description,urlToImage,publishedAt,content):
-        self.name = name
-        self.title = title
+    def __init__(self,name,description,url,category):
+        self.name = name 
         self.description = description
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-        self.content = content
+        self.url = url
+        self.category = category

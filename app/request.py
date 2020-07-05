0286306f02fd9c209c7d66e2c861c10071d166f0
antony-guthiga <toniguthiga93@gmail.com>
2020-07-05 15:41:43 +0300

    add list for sources, add requests to get API

--- app/request.py
+++ app/request.py
@@ -1,6 +1,7 @@
 from app import app
 import urllib.request,json
-from .models import news 
+from .models import news
+import requests 
 
 News = news.News
 
@@ -10,42 +11,42 @@ api_key = app.config['NEWS_API_KEY']
 #Getting the news base url
 base_url = app.config["NEWS_API_BASE_URL"]
 
-def get_news(category):
+def get_news():
     '''
     Function that gets the json response to our url request
     '''
-    get_news_url = base_url.format(category,api_key)
+    get_news_url = base_url.format(api_key)
 
-    with urllib.request.urlopen(get_news_url) as url:
-        get_news_data = url.read()
-        get_news_response = json.loads(get_news_data)
+    get_news_response = requests.get(get_news_url).json()
 
-        news_results = None 
+    news_sources = None 
 
-        if get_news_response['results']:
-            news_results_list = get_news_response['results']
-            news_results = process_results(news_results_list)
+    if get_news_response['sources']:
+        news_sources_list = get_news_response['sources']
+        news_sources = process_sources(news_sources_list)
     
-    return news_results
+    return news_sources
 
-def process_results(news_list):
+def process_sources(news_list):
     '''
-    Function that processes the news result and transform them to a list of Objects
+    Function that processes the news sources and transform them to a list of Objects
 
     Args:
         news_list: A list of dictionaries that contain news details
 
     Returns:
-        news_results: A list of news objects 
+        news_articles: A list of news objects 
     '''
-    news_results = []
+    news_sources = []
     for news_item in news_list:
         name = news_item.get('name')
-        title = news_item.get('title')
-        description = news_item('description')
-        urlToImage = news_item('urlToImage')
-        publishedAt = news_item('publishedAt')
-        content = news_item('content')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        category = news_item.get('category')
 
-    return news_results
+    if name:
+        new_object = News(name,description,url,category)
+        news_sources.append(new_object)
+
+    return news_sources
 

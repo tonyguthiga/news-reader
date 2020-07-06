787b2c5e15d02283d2f4b97ed3608016baf6f7e8
antony-guthiga <toniguthiga93@gmail.com>
2020-07-06 10:57:50 +0300

    add function to request url,process news articles

--- app/request.py
+++ app/request.py
@@ -1,15 +1,15 @@
 from app import app
 import urllib.request,json
-from .models import news
+from app.models import News, Articles
 import requests 
 
-News = news.News
 
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
 
 #Getting the news base url
 base_url = app.config["NEWS_API_BASE_URL"]
+article_base_url = app.config["ARTICLE_API_BASE_URL"]
 
 def get_news():
     '''
@@ -35,18 +35,56 @@ def process_sources(news_list):
         news_list: A list of dictionaries that contain news details
 
     Returns:
-        news_articles: A list of news objects 
+        news_sources: A list of news objects 
     '''
     news_sources = []
     for news_item in news_list:
         name = news_item.get('name')
-        description = news_item.get('description')
         url = news_item.get('url')
-        category = news_item.get('category')
 
-    if name:
-        new_object = News(name,description,url,category)
-        news_sources.append(new_object)
+        if name:
+            new_object = News(name,url)
+            news_sources.append(new_object)
 
     return news_sources
 
+def get_article():
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_article_url = article_base_url.format(api_key)
+
+    get_article_response = requests.get(get_article_url).json()
+
+    news_articles = None 
+
+    if get_article_response['articles']:
+        news_articles_list = get_articles_response['articles']
+        news_articles = process_articles(news_articles_list)
+    
+    return news_articles
+
+def process_articles(articles_list):
+    '''
+    Function that processes the news articles and transform them to a list of Objects
+
+    Args:
+        articles_list: A list of dictionaries that contain news details
+
+    Returns:
+        news_articles: A list of news objects 
+    '''
+    news_articles = []
+    for articles_item in articles_list:
+        author = articles_item.get('author')
+        title = articles_item.get('title')
+        description = articles_item.get('description')
+        url = articles_item.get('url')
+        urlToImage = articles_item.get('urlToImage')
+        publishedAt = articles_item.get('publishedAt')
+
+        if urlToImage:
+            article_object = Articles(author, title, description, url, urlToImage, publishedAt)
+            news_articles.append(article_object)
+
+    return news_articles

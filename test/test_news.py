942309ed5a583f04b20dc5f6009bf268d323d62d
antony-guthiga <toniguthiga93@gmail.com>
2020-07-07 19:22:28 +0300

    add article test

--- test/test_news.py
+++ test/test_news.py
@@ -1,5 +1,5 @@
 import unittest
-from app.models import News
+from app.models import News, Articles
 
 
 class NewsTest(unittest.TestCase):
@@ -15,3 +15,17 @@ class NewsTest(unittest.TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
+
+class ArticlesTest(unittest.TestCase):
+    '''
+    Test Class to  test the behavior of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_articles = Articles('Robert Frank','The billionaires and country clubs that received small business loans from the government - CNBC','Billionaires, country clubs, private jet companies and Kanye West all received millions in government funding under the Paycheck Protection Program, according to filings.','https://www.cnbc.com/2020/07/07/the-billionaires-and-country-clubs-that-received-ppp-loans.html','https://image.cnbcfm.com/api/v1/image/106605723-1594130645953preview.jpg?v=1594130678','2020-07-07T14:18:15Z')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_articles,Articles))
